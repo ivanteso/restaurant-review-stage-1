0712ff5769d22b3a8bd4c185a2b033f63729af84
ivan_teso <ivan.teso@gmail.com>
2018-06-29 15:19:31 +0300

    Feat: add responsive view
    Media queries and flexbox are working now into both
    pages. Modified some classes in .html to work better with CSS and
    modified some JS to build properly the maps after these changes

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -9,7 +9,7 @@ window.initMap = () => {
     if (error) { // Got an error!
       console.error(error);
     } else {
-      self.map = new google.maps.Map(document.getElementById('map'), {
+      self.map = new google.maps.Map(document.querySelector('.map'), {
         zoom: 16,
         center: restaurant.latlng,
         scrollwheel: false
